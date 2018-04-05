0fdf8876fdd7389f2458785362b488a273ab4c02
amusamhe <abdalsamad.y.m@gmail.com>
2018-04-05 14:09:14 +0300

    code refactoring

--- src/serverLogic.js
+++ src/serverLogic.js
@@ -1,6 +1,22 @@
 const words = require('./words.json');
 
+// const matchedWords = (searchWord) => {
+//     return Object.keys(words).filter((word) => word.startsWith(searchWord)).slice(0,10);
+// }
+
 const matchedWords = (searchWord) => {
-    return Object.keys(words).filter((word) => word.startsWith(searchWord)).slice(0,10);
+  let result = [];
+  for (let word of Object.keys(words)) {
+    if (word.startsWith(searchWord)) {
+      result.push(word);
+    }
+    if (result.length >= 10) {
+      break;
+    }
+  }
+  return result;
 }
-  module.exports = {matchedWords};
+
+module.exports = {
+  matchedWords
+};
