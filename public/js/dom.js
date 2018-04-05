57b5a66dd5315a27c10610a10005f689ddd10719
sallamtanna <sallamtanna2015@hotmail.com>
2018-04-05 16:39:39 +0300

    adding alert

--- public/js/dom.js
+++ public/js/dom.js
@@ -1,4 +1,5 @@
 const input = document.querySelector('#searchQuery');
+
 const wordList = document.querySelector('#wordList');
 var send1 = document.getElementById('send');
 var results  = document.getElementById('result');
@@ -59,18 +60,35 @@ xhr.send();
 
 
 
-
 //t htis is the new function ......
 send1.addEventListener('click',function(e){
+  var inputer = document.getElementById('searchQuery').value.length;
   var myUrl1 = 'http://api.wordnik.com/v4/word.json/'+ input.value +'/definitions?limit=1&partOfSpeech=noun&includeRelated=true&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5';
   var myUrl2 = 'http://api.wordnik.com:80/v4/word.json/'+ input.value+'/audio?useCanonical=false&limit=1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5';
 
   e.preventDefault();
 
+  if(inputer === 0){
 
+     alert('Enter some text!');
+  }
+  else{
 fetch100(myUrl1, function(res){
 
-  var data = res.text;
+  if (res === undefined) {
+             document.getElementById('result').innerHTML = '';
+             alert('No results for the word you entered!');
+             document.getElementById('searchQuery').value = '';
+
+         }
+
+
+
+
+else {
+  document.getElementById('searchQuery').value = '';
+         var data = res.text;
+
   var newData = document.createElement('p');
   newData.textContent = data;
   newData.setAttribute('class','p1');
@@ -79,13 +97,15 @@ fetch100(myUrl1, function(res){
   fetch100(myUrl2, function(res2){
     var mySoundUrl = res2.fileUrl;
     var audio = document.createElement('audio');
+    audio.setAttribute('class', 'mysound');
     audio.setAttribute('class','myDiv');
     var sound = document.createElement('source');
     sound.setAttribute('src',mySoundUrl);
     audio.setAttribute('controls','controls');
     audio.appendChild(sound);
     results.appendChild(audio);
-  });
+  });}
 });
+}
 document.getElementById("result").textContent = " ";
 })
