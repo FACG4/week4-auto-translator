158a3b67738494210d5d34dc13cc23869f48da10
amusamhe <abdalsamad.y.m@gmail.com>
2018-04-05 16:13:52 +0300

    last edits

--- public/js/dom.js
+++ public/js/dom.js
@@ -1,17 +1,14 @@
 const input = document.querySelector('#searchQuery');
 const wordList = document.querySelector('#wordList');
-var send1 = document.getElementById('send');
-var results  = document.getElementById('result');
+const translateBtn = document.getElementById('send');
+const results = document.getElementById('result');
 
 input.addEventListener('keyup', () => {
-  fetch(input.value.toLowerCase(), render);
+  fetch('POST', '/search', input.value.toLowerCase(), render);
 });
 
-
-
-
-
-function fetch(str, cb) {
+// generic fetch function
+function fetch(method, url, payload, cb) {
   const xhr = new XMLHttpRequest;
   xhr.onreadystatechange = function () {
     if (xhr.readyState === 4 && xhr.status === 200) {
@@ -19,15 +16,9 @@ function fetch(str, cb) {
       cb(data);
     }
   }
-  xhr.open('POST', "/search");
-  xhr.send(str);
+  xhr.open(method, url);
+  xhr.send(payload);
 }
-
-
-
-
-
-
 
 function render(arr) {
   const autoList = document.createElement('datalist');
@@ -42,50 +33,33 @@ function render(arr) {
   wordList.replaceChild(autoList, oldList);
 }
 
+// Getting the info form the API
+translateBtn.addEventListener('click', function (e) {
+  const myUrl1 = 'https://cors-anywhere.herokuapp.com/http://api.wordnik.com/v4/word.json/' + input.value + '/definitions?limit=1&partOfSpeech=noun&includeRelated=true&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5';
+  const myUrl2 = 'https://cors-anywhere.herokuapp.com/http://api.wordnik.com:80/v4/word.json/' + input.value + '/audio?useCanonical=false&limit=1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5';
 
-function fetch100(url, callback) {
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function() {
-if (xhr.readyState == 4 && xhr.status == 200) {
- response = JSON.parse(xhr.responseText)[0];
-callback(response);
-}
-}
-xhr.open('GET', url)
-xhr.send();
-}
+  fetch('GET', myUrl1, null, function (res) {
+    if(res[0]==='undefined' || input.value.length === 0) {
+      alert('Unavailable Translation');
+      return;
+    }
+    const data = res[0].text;    
+    const newData = document.createElement('p');
+    newData.textContent = data;
+    newData.setAttribute('class', 'p1');
+    results.appendChild(newData)
 
-
-
-
-
-
-//t htis is the new function ......
-send1.addEventListener('click',function(e){
-  var myUrl1 = 'http://api.wordnik.com/v4/word.json/'+ input.value +'/definitions?limit=1&partOfSpeech=noun&includeRelated=true&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5';
-  var myUrl2 = 'http://api.wordnik.com:80/v4/word.json/'+ input.value+'/audio?useCanonical=false&limit=1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5';
-
-  e.preventDefault();
-
-
-fetch100(myUrl1, function(res){
-
-  var data = res.text;
-  var newData = document.createElement('p');
-  newData.textContent = data;
-  newData.setAttribute('class','p1');
-  results.appendChild(newData)
-
-  fetch100(myUrl2, function(res2){
-    var mySoundUrl = res2.fileUrl;
-    var audio = document.createElement('audio');
-    audio.setAttribute('class','myDiv');
-    var sound = document.createElement('source');
-    sound.setAttribute('src',mySoundUrl);
-    audio.setAttribute('controls','controls');
-    audio.appendChild(sound);
-    results.appendChild(audio);
+    fetch('GET', myUrl2, null, function (res2) {
+      const mySoundUrl = res2[0].fileUrl;
+      const audio = document.createElement('audio');
+      audio.setAttribute('class', 'myDiv');
+      const sound = document.createElement('source');
+      sound.setAttribute('src', mySoundUrl);
+      audio.setAttribute('controls', 'controls');
+      audio.appendChild(sound);
+      results.appendChild(audio);
+    });
   });
-});
-document.getElementById("result").textContent = " ";
+  document.getElementById("result").textContent = " ";
+  input.value = '';
 })
